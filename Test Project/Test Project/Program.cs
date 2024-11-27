244894e780af386d5c365d246448dd5d40097e4a
Shevelin75 <shevelin.oper75@gmail.com>
2024-11-27 22:39:56 +0900

    Add files via upload

---
+++ Test Project/Test Project/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using InspectorLib;
+
+namespace Test_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FunctionInsp inspector = new FunctionInsp();
+
+            Console.WriteLine($"Главный инспектор: {inspector.GetInspector()}");
+            Console.WriteLine($"Название автоинспекции: {inspector.GetCarInspection()}");
+
+            bool inspectorChanged = inspector.SetInspector("Миронов А.В.");
+            Console.WriteLine($"Инспектор изменен: {inspectorChanged}");
+            Console.WriteLine($"Новый главный инспектор: {inspector.GetInspector()}");
+
+            int randomNumber = inspector.GenerateRandomNumber();
+            string generatedNumber = inspector.GenerateNumber('А', randomNumber);
+            Console.WriteLine($"Сгенерированный госномер: {generatedNumber}");
+
+            List<string> workersList = inspector.GetWorker();
+            Console.WriteLine("Список сотрудников:");
+            foreach (string worker in workersList)
+            {
+                Console.WriteLine(worker);
+            }
+
+            bool workerAdded = inspector.AddWorker("Смирнов П.П.");
+            Console.WriteLine($"Сотрудник добавлен: {workerAdded}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
